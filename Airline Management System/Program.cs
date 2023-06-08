fd8ba93d98beb5ee5f47e6d4e0f33b5d560513e3
Usaid Ur Rehman <usaid.rehman187@gmail.com>
2023-06-08 14:42:26 +0500

    Instance work done

--- Airline Management System/Program.cs
+++ Airline Management System/Program.cs
@@ -3,20 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 namespace Airline_Management_System
 {
-    static class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace Project_1st_
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static class Program
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Server());
+            /// <summary>
+            /// The main entry point for the application.
+            /// </summary>
+            private static readonly string MutexId = "YourApplicationUniqueMutexId";
+
+            [STAThread]
+            static void Main()
+            {
+                using (Mutex mutex = new Mutex(true, MutexId, out bool createdNew))
+                {
+                    // Check if this instance of the application is already running
+                    if (!createdNew)
+                    {
+                        // Another instance is running, exit the application
+                        MessageBox.Show("Another instance of the application is already running.", "AirLineManagementSystem",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Application.Exit();
+                        return;
+                    }
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Server());
+                }
+            }
         }
     }
 }
